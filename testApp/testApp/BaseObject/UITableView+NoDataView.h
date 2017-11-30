dbe1e6662cf3bbb301f47dcd5e17e39edfa34cda
cg <375027175@qq.com>
2017-11-30 14:10:01 +0800

    注释

--- testApp/testApp/BaseObject/UITableView+NoDataView.h
+++ testApp/testApp/BaseObject/UITableView+NoDataView.h
@@ -11,10 +11,14 @@
 typedef BOOL (^CompareBlock)(void);
 
 @interface UITableView (NoDataView)
-+ (void)replaceMethod; //替换方法， AppDelegate 中运行一次
+//替换方法， AppDelegate 中运行一次
++ (void)replaceMethod;
 
+//设置无数据展示的信息
 - (void)setShowMessage:(NSString *)showMessage;
 
+// 注意循环引用!!!!!
+// block 返回 BOOL 怎么比较。  YES 显示无数据页面， NO 隐藏无数据页面
 - (void)setShowNoDataViewCompara:(CompareBlock) block;
 
 @end
